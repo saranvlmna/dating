944960836fc55409b0e9e5e39de1650574a19919
saranjith <saranjith@metrictreelabs.com>
2023-03-03 16:39:22 +0530

    iterest update

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { Interests } from "src/database/interests";
+import { Social, SocialDocumet } from "src/database/social";
 import { User, UserDocumet } from "src/database/user";
+
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocumet>,
-    private jwtService: JwtService
-  ) {}
+    @InjectModel(Social.name) private socialModel: Model<SocialDocumet>,
+    @InjectModel(Interests.name) private interestsModel: Model<SocialDocumet>,
+    
+  ) { }
 
   async findAll() {
     return await this.userModel.find();
@@ -22,4 +27,32 @@ export class UserService {
   async deleteUser(id: any) {
     return this.userModel.deleteOne({ id: id });
   }
+
+  async updateSocialLinks(data) {
+    let existSocialData = await this.socialModel.findOne({ userId: data.userId })
+    console.log(existSocialData);
+    
+    if (!existSocialData) {
+      return await this.socialModel.create(data)
+    }
+    else {
+      return await this.socialModel.updateOne({
+        userId: data.userId
+      },data)
+    }
+  }
+
+  async updateInterests(data) {
+    let existInterests = await this.interestsModel.findOne({ userId: data.userId })
+    if (!existInterests) {
+      return await this.interestsModel.create(data)
+    }
+    else {
+      return await this.interestsModel.updateOne({
+        userId: data.userId
+      }, data)
+    }
+  }
+
+  
 }
